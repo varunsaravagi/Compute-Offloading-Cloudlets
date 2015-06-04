51e45108805fb81bb809dd629a425d5265f9ca5b
varun <varunsaravagi@gmail.com>
2015-06-03 23:18:53 -0400

    added comments to the javascript. Variables can be controlled from the main screen now

--- TearableCloth/Cloth.js
+++ TearableCloth/Cloth.js
@@ -25,15 +25,34 @@ function getValue(elementId){
     return document.getElementById(elementId).value;
 }
 
-var physics_accuracy = document.getElementById('pa_text').value,//getValue("physics_accuracy"),
-    mouse_influence = document.getElementById('mi_text').value,//20,//getValue("mouse_influence"),
-    mouse_cut = document.getElementById('mc_text').value,//5,
-    gravity = document.getElementById('g_text').value,//1200,
-    cloth_height = document.getElementById('ch_text').value,// 30,
-    cloth_width = 50,//document.getElementById('cw_text').value,//50,
-    start_y = 20,//document.getElementById('sy_text').value,//20,
-    spacing = 7,//document.getElementById('s_text').value,//7,
-    tear_distance = 60;//document.getElementById('td_text').value;//60;
+// parameters required for simulation
+
+// attempt to get the parameters from the HTML page. Giving error from cloth_width onwards.
+
+var physics_accuracy,
+    mouse_influence,
+    mouse_cut,
+    gravity,
+    cloth_height,
+    cloth_width,
+    start_y,
+    spacing,
+    tear_distance;
+
+function load_variables(){
+    physics_accuracy = parseInt(document.getElementById('pa_text').value),//getValue("physics_accuracy"),
+    mouse_influence = parseInt(document.getElementById('mi_text').value),//20,//getValue("mouse_influence"),
+    mouse_cut = parseInt(document.getElementById('mc_text').value),//5,
+    gravity = parseInt(document.getElementById('g_text').value),//1200,
+    cloth_height = parseInt(document.getElementById('ch_text').value),// 30,
+    cloth_width = parseInt(document.getElementById('cw_text').value),//50,
+    start_y = parseInt(document.getElementById('sy_text').value),//20,
+    spacing = parseInt(document.getElementById('s_text').value),//7,
+    tear_distance = parseInt(document.getElementById('td_text').value);//60;
+    canvas.width = parseInt(document.getElementById('caw_text').value);
+    canvas.height = parseInt(document.getElementById('cah_text').value);
+
+};
 
 window.requestAnimFrame =
     window.requestAnimationFrame ||
@@ -59,7 +78,7 @@ var canvas,
         py: 0
     };
 
-// Define a point
+// Define a point and initialize certain properties
 var Point = function (x, y) {
 
     this.x = x;
@@ -93,6 +112,7 @@ Point.prototype.update = function (delta) {
         } else if (dist < mouse_cut) this.constraints = [];
     }
 
+    // add gravity in the y direction
     this.add_force(0, gravity);
 
     delta *= delta;
@@ -108,16 +128,22 @@ Point.prototype.update = function (delta) {
     this.vy = this.vx = 0
 };
 
+// draw the point
 Point.prototype.draw = function () {
 
+    // Do not draw the top most points
     if (this.constraints.length <= 0) return;
 
     var i = this.constraints.length;
-    while (i--) this.constraints[i].draw();
+    // Draw points for each constraint this point has
+    while (i--) 
+        this.constraints[i].draw();
 };
 
+// Resolve the constraints of the point with the attached points
 Point.prototype.resolve_constraints = function () {
 
+    // if the point is a top-row point, return
     if (this.pin_x != null && this.pin_y != null) {
 
         this.x = this.pin_x;
@@ -125,15 +151,18 @@ Point.prototype.resolve_constraints = function () {
         return;
     }
 
+    // Get the number of constraints. Would be maximum 2
     var i = this.constraints.length;
-    while (i--) this.constraints[i].resolve();
+    while (i--) 
+        this.constraints[i].resolve();
+
+    // if the point is going outside the boundary, 
+    // give it a position within the boundary.
 
     if (this.x > boundsx) {
-
         this.x = 2 * boundsx - this.x;
         
     } else if (this.x < 1) {
-
         this.x = 2 - this.x;
     }
 
@@ -147,6 +176,9 @@ Point.prototype.resolve_constraints = function () {
     }
 };
 
+// add the input point as a constraint to this point.
+// A point is attached to atmost 2 points:
+// one on its left and one immediately above it.
 Point.prototype.attach = function (point) {
 
     this.constraints.push(
@@ -154,6 +186,7 @@ Point.prototype.attach = function (point) {
     );
 };
 
+// remove the given link from the point
 Point.prototype.remove_constraint = function (lnk) {
 
     var i = this.constraints.length;
@@ -161,12 +194,15 @@ Point.prototype.remove_constraint = function (lnk) {
         if (this.constraints[i] == lnk) this.constraints.splice(i, 1);
 };
 
+// add force on the point in both the x and y directions
 Point.prototype.add_force = function (x, y) {
 
     this.vx += x;
     this.vy += y;
 };
 
+// Set the coordinates where the point is to be pinned
+// Only applicable for the top row points of the cloth
 Point.prototype.pin = function (pinx, piny) {
     this.pin_x = pinx;
     this.pin_y = piny;
@@ -179,20 +215,28 @@ var Constraint = function (p1, p2) {
     this.length = spacing;
 };
 
+// Resolve the constraint
 Constraint.prototype.resolve = function () {
 
+    // Get the new distance between the two points in the constraint
     var diff_x = this.p1.x - this.p2.x,
         diff_y = this.p1.y - this.p2.y,
         dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y),
+        //Get the difference from the length. No idea why this formula was used
         diff = (this.length - dist) / dist;
 
+    // if distance between points is > tear distance, remove the constraint,
+    // i.e detach the points     
     if (dist > tear_distance) this.p1.remove_constraint(this);
 
+    // calculate the amount by which positions are to be changed.
     var px = diff_x * diff * 0.5;
     var py = diff_y * diff * 0.5;
 
+    // add the difference to first point
     this.p1.x += px;
     this.p1.y += py;
+    // subtract the difference from second point
     this.p2.x -= px;
     this.p2.y -= py;
 };
@@ -205,18 +249,31 @@ Constraint.prototype.draw = function () {
 
 var Cloth = function () {
 
+    // store all the points in the cloth
     this.points = [];
 
     var start_x = canvas.width / 2 - cloth_width * spacing / 2;
 
+    // generate point for every combination of (x,y)
     for (var y = 0; y <= cloth_height; y++) {
 
         for (var x = 0; x <= cloth_width; x++) {
 
+            //Position of the point
             var p = new Point(start_x + x * spacing, start_y + y * spacing);
 
+            // if it is not the first point in the row, attach it with the point just before it
             x != 0 && p.attach(this.points[this.points.length - 1]);
+            
+            // if it is the first row, pin the point at the coordinate. This is to keep the cloth
+            // attached from the top.
             y == 0 && p.pin(p.x, p.y);
+            
+            // if it is not the first row, attach the point to the point just above it in the matrix
+            // |.|.|.|.|.|
+            // |.|.|i|.|.|
+            // |.|.|p|.|.|
+            // attach p to i.
             y != 0 && p.attach(this.points[x + (y - 1) * (cloth_width + 1)])
 
             this.points.push(p);
@@ -224,28 +281,38 @@ var Cloth = function () {
     }
 };
 
+// update the points in the cloth : COMPUTATION
 Cloth.prototype.update = function () {
 
     var i = physics_accuracy;
 
+    // Resolve the constraints for all the points physics_accuracy number of times
     while (i--) {
         var p = this.points.length;
-        while (p--) this.points[p].resolve_constraints();
+        while (p--) 
+            this.points[p].resolve_constraints();
     }
 
+
     i = this.points.length;
-    while (i--) this.points[i].update(.016);
+    // update all the points by delta amount. Brings swaying motion to the cloth
+    while (i--) 
+        this.points[i].update(.016);
 };
 
+// Draw the cloth
 Cloth.prototype.draw = function () {
 
     ctx.beginPath();
 
     var i = cloth.points.length;
-    while (i--) cloth.points[i].draw();
+    // Loop over all the points and draw each point
+    while (i--) 
+        cloth.points[i].draw();
 
     ctx.stroke();
 };
+
 
 function update() {
 
@@ -292,6 +359,9 @@ function start() {
     boundsy = canvas.height - 1;
 
     ctx.strokeStyle = '#888';
+    
+    load_variables();    
+    // Define the points and constraints in the cloth
     cloth = new Cloth();
     update();
 }
@@ -303,7 +373,6 @@ window.onload = function () {
 
     canvas.width = 560;
     canvas.height = 350;
-
 
     start();
 };
