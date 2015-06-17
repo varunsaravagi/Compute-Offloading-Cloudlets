2d5c09685706996334c0404998b771f66eb51b35
varun saravagi <varunsaravagi@gmail.com>
2015-06-16 21:34:04 -0400

    moved initialization to server, client only draws

--- TearableCloth_Partitioned/cloth_server.js
+++ TearableCloth_Partitioned/cloth_server.js
@@ -1,4 +1,3 @@
-
  var http = require('http');
  var fs = require('fs');
  var path = require('path');
@@ -6,86 +5,49 @@
  var msgpack = require('msgpack-js-browser');
 
  function handler(req, res) {
- 	var filePath = __dirname + '/index.html';
+    var filePath = __dirname + '/index.html';
 
-	if(req.url == '/')
- 		filePath = __dirname + '/index.html';
- 	
- 	//console.log('Request URL :' + req.url);
- 	var extName = path.extname(req.url);
- 	var contentType = 'text/html';
+    if(req.url == '/')
+        filePath = __dirname + '/index.html';
+    
+    //console.log('Request URL :' + req.url);
+    var extName = path.extname(req.url);
+    var contentType = 'text/html';
 
- 	switch(extName){
- 		case '.js':
- 			contentType = 'text/javascript';
- 			filePath = __dirname + req.url;
- 			break;
- 		case '.css':
- 			contentType = 'text/css';
- 			filePath = __dirname + req.url;
- 			break;
- 		default:
- 			contentType = 'text/html';
- 			filePath = __dirname + '/index.html';
- 			break;
- 	}
- 	//console.log('File Path :' + filePath);
- 	if(req.url != '/favicon.ico'){
- 		//console.log("not favicon")
- 		fs.readFile(filePath, function(err, data) {
- 			if (err) {
- 				console.log(err);
- 				res.writeHead(500);
- 				return res.end('Error loading index.html');
- 			}
- 			console.log('Connection successful');
- 			res.writeHead(200, {'Content-Type': contentType});
-			res.end(data, 'utf-8');
-		});
- 	}
-
-};
-
-
-server.listen(1234);
-var io = require('socket.io').listen(server);
-// Functions to run on the server side
-var parameters;
-var cloth = {
-    points: []
+    switch(extName){
+        case '.js':
+            contentType = 'text/javascript';
+            filePath = __dirname + req.url;
+            break;
+        case '.css':
+            contentType = 'text/css';
+            filePath = __dirname + req.url;
+            break;
+        default:
+            contentType = 'text/html';
+            filePath = __dirname + '/index.html';
+            break;
+    }
+    //console.log('File Path :' + filePath);
+    if(req.url != '/favicon.ico'){
+        //console.log("not favicon")
+        fs.readFile(filePath, function(err, data) {
+            if (err) {
+                console.log(err);
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+            console.log('Connection successful');
+            res.writeHead(200, {'Content-Type': contentType});
+            res.end(data, 'utf-8');
+        });
+    }
 
 };
-io.sockets.on('connection', function(socket){
-	
-	socket.on('load_parameters', function(data){
-		parameters = data.parameters;
-		console.log("Parameters received. physics_accuracy = " + parameters.physics_accuracy);
-        // cloth = new Cloth();
-        // console.log('Cloth length: ' + cloth.points.length)
-        // console.log('Point 0:');
-        // console.log(cloth.points[0]);
-        // console.log('Point 1:');
-        // console.log(cloth.points[1]);
-        // console.log('Point 1 constraints:');
-        // console.log(cloth.points[1].constraints[0]);
-        // var encoded = msgpack.encode(cloth.points[1]);
-	});
 
-    socket.on('update_cloth', function(data){
-        cloth.points.push(data.point);
-        console.log('Received length: ' + cloth.points.length);
-        //var cloth = data.cloth;
-        //console.log('hello');
-        //console.log(data.cloth.x);
-    });
-
-}); // end io.sockets.on
-
-// Copied Client code to test serialization via msgpack.
-// Gives call stack size exceeded error
 
 // Define a point and initialize certain properties
-var Point = function (x, y) {
+var Point = function (x, y, index) {
 
     this.x = x;
     this.y = y;
@@ -95,32 +57,189 @@ var Point = function (x, y) {
     this.vy = 0;
     this.pin_x = null;
     this.pin_y = null;
+    this.index = 0;
+    this.constraints = [];
+};
+
+// Point data structure to pass to client
+var SerializedPoint = function (point){
+    this.index = point.index;
+    this.x = point.x;
+    this.y = point.y;
+    this.constraints = [];
+    var i = point.constraints.length;
+    while(i--){
+        this.constraints[i] = {x: point.constraints[i].p2.x, y: point.constraints[i].p2.y};
+    }
+};
+
+var SerializedCloth = function(){
+
+    this.points = [];
+};
+
+
+// parameters required for simulation
+var canvas,
+    ctx,
+    cloth,
+    boundsx,
+    boundsy,
+    mouse = {
+        down: false,
+        button: 1,
+        x: 0,
+        y: 0,
+        px: 0,
+        py: 0
+    };
+
+var parameters;
+var sCloth;
+
+
+
+server.listen(1234);
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+    
+    socket.on('load_parameters', function(data){
+        parameters = data.parameters;
+        console.log("Parameters received. physics_accuracy = " + parameters.physics_accuracy);
+        sCloth = new SerializedCloth();
+        cloth = new Cloth();
+        console.log('Cloth length: ' + cloth.points.length)
+        console.log('Serialized Cloth length: ' + sCloth.points.length)
+        cloth.update();
+        var length = sCloth.points.length;
+        var encoded = msgpack.encode(sCloth);
+        console.log('Encoded cloth');
+        socket.emit('updatedCloth', {cloth : encoded});
+    });
+}); // end io.sockets.on
+
+
+SerializedPoint.prototype.update = function(point){
+    this.x = point.x;
+    this.y = point.y;
 
     this.constraints = [];
+    var i = point.constraints.length;
+    while(i--){
+        this.constraints[i] = {x: point.constraints[i].p2.x, y: point.constraints[i].p2.y};
+    }
+}
+
+SerializedCloth.prototype.update = function(point, index){
+    this.points[index].update(point);
+}
+
+// Add update property to Point
+Point.prototype.update = function (delta) {
+
+    if (mouse.down) {
+
+        var diff_x = this.x - mouse.x,
+            diff_y = this.y - mouse.y,
+            dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y);
+
+        if (mouse.button == 1) {
+
+            if (dist < mouse_influence) {
+                this.px = this.x - (mouse.x - mouse.px) * 1.8;
+                this.py = this.y - (mouse.y - mouse.py) * 1.8;
+            }
+
+        } else if (dist < mouse_cut) this.constraints = [];
+    }
+
+    // add gravity in the y direction
+    this.add_force(0, parameters.gravity);
+
+    delta *= delta;
+    nx = this.x + ((this.x - this.px) * .99) + ((this.vx / 2) * delta);
+    ny = this.y + ((this.y - this.py) * .99) + ((this.vy / 2) * delta);
+
+    this.px = this.x;
+    this.py = this.y;
+
+    this.x = nx;
+    this.y = ny;
+
+    this.vy = this.vx = 0
+
+
+};
+
+// Resolve the constraints of the point with the attached points
+Point.prototype.resolve_constraints = function () {
+
+    // if the point is a top-row point, return
+    if (this.pin_x != null && this.pin_y != null) {
+
+        this.x = this.pin_x;
+        this.y = this.pin_y;
+        return;
+    }
+
+    // Get the number of constraints. Would be maximum 2
+    var i = this.constraints.length;
+    while (i--) 
+        this.constraints[i].resolve();
+
+    // if the point is going outside the boundary, 
+    // give it a position within the boundary.
+
+    if (this.x > boundsx) {
+        this.x = 2 * boundsx - this.x;
+        
+    } else if (this.x < 1) {
+        this.x = 2 - this.x;
+    }
+
+    if (this.y > boundsy) {
+
+        this.y = 2 * boundsy - this.y;
+        
+    } else if (this.y < 1) {
+
+        this.y = 2 - this.y;
+    }
 };
 
 // add the input point as a constraint to this point.
 // A point is attached to atmost 2 points:
 // one on its left and one immediately above it.
 Point.prototype.attach = function (point) {
-    console.log('Attached to point(' + this.x + ', ' + this.y + '), point(' + point.x + ', ' + point.y + ')');
-    // add the point as a constraint 
-    this.constraints.push(point);
-    // this.constraints.push(
-    //     new Constraint(this, point)
-    // );
+
+    this.constraints.push(
+        new Constraint(this, point)
+    );
+};
+
+// remove the given link from the point
+Point.prototype.remove_constraint = function (lnk) {
+
+    var i = this.constraints.length;
+    while (i--)
+        if (this.constraints[i] == lnk) this.constraints.splice(i, 1);
+};
+
+// add force on the point in both the x and y directions
+Point.prototype.add_force = function (x, y) {
+
+    this.vx += x;
+    this.vy += y;
 };
 
 // Set the coordinates where the point is to be pinned
 // Only applicable for the top row points of the cloth
 Point.prototype.pin = function (pinx, piny) {
-    console.log('Pinned point (' + this.x + ', ' + this.y + ') at (' + pinx + ', ' + piny);
     this.pin_x = pinx;
     this.pin_y = piny;
 };
 
-
-// Initialize constraint
 var Constraint = function (p1, p2) {
 
     this.p1 = p1;
@@ -128,26 +247,52 @@ var Constraint = function (p1, p2) {
     this.length = parameters.spacing;
 };
 
+// Resolve the constraint
+Constraint.prototype.resolve = function () {
+
+    // Get the new distance between the two points in the constraint
+    var diff_x = this.p1.x - this.p2.x,
+        diff_y = this.p1.y - this.p2.y,
+        dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y),
+        //Get the difference from the length. No idea why this formula was used
+        diff = (this.length - dist) / dist;
+
+    // if distance between points is > tear distance, remove the constraint,
+    // i.e detach the points     
+    if (dist > parameters.tear_distance) 
+        this.p1.remove_constraint(this);
+
+    // calculate the amount by which positions are to be changed.
+    var px = diff_x * diff * 0.5;
+    var py = diff_y * diff * 0.5;
+
+    // add the difference to first point
+    this.p1.x += px;
+    this.p1.y += py;
+    // subtract the difference from second point
+    this.p2.x -= px;
+    this.p2.y -= py;
+};
+
 var Cloth = function () {
 
     // store all the points in the cloth
     this.points = [];
 
-    var start_x = 560 / 2 - parameters.cloth_width * parameters.spacing / 2;
-
+    var start_x = parameters.canvas_width / 2 - parameters.cloth_width * parameters.spacing / 2;
+    var index;
     // generate point for every combination of (x,y)
-    for (var y = 0; y <= 2; y++) {
-        console.log('y: ' + y);
-        console.log('------------');
-        for (var x = 0; x <= 2; x++) {
-            console.log('x: ' + x)
+    for (var y = 0; y <= parameters.cloth_height; y++) {
+
+        for (var x = 0; x <= parameters.cloth_width; x++) {
+
             //Position of the point
-            var p = new Point(start_x + x * parameters.spacing, parameters.start_y + y * parameters.spacing);
-            console.log('Point created ' + p.x + ', ' + p.y);
+            index = (y*(parameters.cloth_width+1)) + x;
+            var p = new Point(start_x + x * parameters.spacing, parameters.start_y + y * parameters.spacing, index);
 
             // if it is not the first point in the row, attach it with the point just before it
             x != 0 && p.attach(this.points[this.points.length - 1]);
-
+            
             // if it is the first row, pin the point at the coordinate. This is to keep the cloth
             // attached from the top.
             y == 0 && p.pin(p.x, p.y);
@@ -160,146 +305,49 @@ var Cloth = function () {
             y != 0 && p.attach(this.points[x + (y - 1) * (parameters.cloth_width + 1)])
 
             this.points.push(p);
-            console.log('Constraints:' + p.constraints);
+            sCloth.points.push(new SerializedPoint(p));
         }
     }
 };
 
+// update the points in the cloth : COMPUTATION
+Cloth.prototype.update = function () {
 
-// Add update property to Point
-function update_point(point, delta) {
-
-    if (mouse.down) {
-
-        var diff_x = point.x - mouse.x,
-            diff_y = point.y - mouse.y,
-            dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y);
-
-        if (mouse.button == 1) {
-
-            if (dist < mouse_influence) {
-                point.px = point.x - (mouse.x - mouse.px) * 1.8;
-                point.py = point.y - (mouse.y - mouse.py) * 1.8;
-            }
-
-        } else if (dist < mouse_cut) point.constraints = [];
-    }
-
-    // add gravity in the y direction
-    point = add_force(0, gravity);
-
-    delta *= delta;
-    nx = point.x + ((point.x - point.px) * .99) + ((point.vx / 2) * delta);
-    ny = point.y + ((point.y - point.py) * .99) + ((point.vy / 2) * delta);
-
-    point.px = point.x;
-    point.py = point.y;
-
-    point.x = nx;
-    point.y = ny;
-
-    point.vy = point.vx = 0
-
-    return point;
-};
-
-
-function add_force(point, x, y){
-	point.vx += x;
-	point.vy += y;
-	return point;
-}
-
-function resolve_point_constraints(point){
-
-    // if the point is a top-row point, return
-    if (point.pin_x != null && point.pin_y != null) {
-
-        point.x = point.pin_x;
-        point.y = point.pin_y;
-        return;
-    }
-
-    // Get the number of constraints. Would be maximum 2
-    var i = point.constraints.length;
-    while (i--) 
-    	point.constraints[i] = resolve_constraint(point.constraints[i]);
-
-    // if the point is going outside the boundary, 
-    // give it a position within the boundary.
-
-    if (point.x > boundsx) {
-        point.x = 2 * boundsx - point.x;
-        
-    } else if (point.x < 1) {
-        point.x = 2 - point.x;
-    }
-
-    if (point.y > boundsy) {
-
-        point.y = 2 * boundsy - point.y;
-        
-    } else if (point.y < 1) {
-
-        point.y = 2 - point.y;
-    }
-}
-
-// Resolve the constraint
-function resolve_constraint(constraint) {
-
-    // Get the new distance between the two points in the constraint
-    var diff_x = constraint.p1.x - constraint.p2.x,
-        diff_y = constraint.p1.y - constraint.p2.y,
-        dist = Math.sqrt(diff_x * diff_x + diff_y * diff_y),
-        //Get the difference from the length. No idea why this formula was used
-        diff = (constraint.length - dist) / dist;
-
-    // if distance between points is > tear distance, remove the constraint,
-    // i.e detach the points     
-    if (dist > tear_distance) 
-    	constraint.p1 = remove_constraint(constraint.p1, constraint);
-
-    // calculate the amount by which positions are to be changed.
-    var px = diff_x * diff * 0.5;
-    var py = diff_y * diff * 0.5;
-
-    // add the difference to first point
-    constraint.p1.x += px;
-    constraint.p1.y += py;
-    // subtract the difference from second point
-    constraint.p2.x -= px;
-    constraint.p2.y -= py;
-    return constraint;
-};
-
-// remove the given link from the point
-function remove_constraint(point, constraint){
-
-    var i = point.constraints.length;
-    while (i--)
-        if (point.constraints[i] == constraint) 
-        	return point.constraints.splice(i, 1);
-};
-
-// update the cloth based on mouse events being performed on screen
-function update_cloth(cloth_local) {
-	var i = parameters.physics_accuracy;
+    var i = parameters.physics_accuracy;
 
     // Resolve the constraints for all the points physics_accuracy number of times
     while (i--) {
-        var p = cloth_local.points.length;
+        var p = this.points.length;
         while (p--) 
-            resolve_point_constraints(cloth_local.points[p]);
+            this.points[p].resolve_constraints();
     }
 
-    i = cloth_local.points.length;
-    
-    // update all the points by delta amount.
-    while (i--) 
-        cloth_local.points[i] = update_point(cloth_local.points[i], .016);
 
-    return cloth_local;
+    i = this.points.length;
+    // update all the points by delta amount. Brings swaying motion to the cloth
+    while (i--) 
+        this.points[i].update(.016);
+};
+
+// Draw the cloth
+Cloth.prototype.draw = function () {
+
+    ctx.beginPath();
+
+    var i = cloth.points.length;
+    // Loop over all the points and draw each point
+    while (i--) 
+        cloth.points[i].draw();
+
+    ctx.stroke();
 };
 
 
+function update() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    cloth.update();
+    cloth.draw();
+
+}
--- TearableCloth_Partitioned/js/cloth_client.js
+++ TearableCloth_Partitioned/js/cloth_client.js
@@ -11,7 +11,9 @@ var parameters = {
     cloth_width : 50,
     start_y : 20,
     spacing : 7,
-    tear_distance : 60
+    tear_distance : 60,
+    canvas_width : 0,
+    canvas_height : 0
 };
 
 window.requestAnimFrame =
@@ -51,180 +53,41 @@ function load_variables(){
     parameters.tear_distance = parseInt(document.getElementById('td_text').value);//60;
     canvas.width = parseInt(document.getElementById('caw_text').value);
     canvas.height = parseInt(document.getElementById('cah_text').value);
+    parameters.canvas_width = canvas.width;
+    parameters.canvas_height = canvas.height;
     socket.emit('load_parameters', {'parameters' : parameters});
 };
 
-// Define a point and initialize certain properties
-var Point = function (x, y) {
-
-    this.x = x;
-    this.y = y;
-    this.px = x;
-    this.py = y;
-    this.vx = 0;
-    this.vy = 0;
-    this.pin_x = null;
-    this.pin_y = null;
-
-    this.constraints = [];
-};
-
-// add the input point as a constraint to this point.
-// A point is attached to atmost 2 points:
-// one on its left and one immediately above it.
-Point.prototype.attach = function (point) {
-    this.constraints.push(point);
-    // this.constraints.push(
-    //     new Constraint(this, point)
-    // );
-};
-
-// Set the coordinates where the point is to be pinned
-// Only applicable for the top row points of the cloth
-Point.prototype.pin = function (pinx, piny) {
-    this.pin_x = pinx;
-    this.pin_y = piny;
-};
-
-// draw the point
-Point.prototype.draw = function () {
-
-    // Do not draw the top most points
-    if (this.constraints.length <= 0) 
-        return;
-
-    var i = this.constraints.length;
-    // Draw points for each constraint this point has
-    while (i--) 
-        this.constraints[i].draw();
-};
-
-// Initialize constraint
-var Constraint = function (p1, p2) {
-
-    this.p1 = p1;
-    this.p2 = p2;
-    this.length = spacing;
-};
-
-
-Constraint.prototype.draw = function () {
-
-    ctx.moveTo(this.p1.x, this.p1.y);
-    ctx.lineTo(this.p2.x, this.p2.y);
-};
-
-
-// Define a cloth
-var Cloth = function () {
-
-    // store all the points in the cloth
-    this.points = [];
-
-    var start_x = canvas.width / 2 - parameters.cloth_width * parameters.spacing / 2;
-
-    // generate point for every combination of (x,y)
-    for (var y = 0; y <= parameters.cloth_height; y++) {
-
-        for (var x = 0; x <= parameters.cloth_width; x++) {
-
-            //Position of the point
-            var p = new Point(start_x + x * parameters.spacing, start_y + y * parameters.spacing);
-
-            // if it is not the first point in the row, attach it with the point just before it
-            x != 0 && p.attach(this.points[this.points.length - 1]);
-
-            // if it is the first row, pin the point at the coordinate. This is to keep the cloth
-            // attached from the top.
-            y == 0 && p.pin(p.x, p.y);
-            
-            // if it is not the first row, attach the point to the point just above it in the matrix
-            // |.|.|.|.|.|
-            // |.|.|i|.|.|
-            // |.|.|p|.|.|
-            // attach p to i.
-            y != 0 && p.attach(this.points[x + (y - 1) * (parameters.cloth_width + 1)])
-
-            this.points.push(p);
-        }
-    }
-};
-
-
+socket.on('updatedCloth', function(data){
+    cloth = msgpack.decode(data.cloth);
+    console.log(cloth.points.length);
+    draw();
+});
 
 
 // Draw the cloth
-Cloth.prototype.draw = function () {
+function draw() {
 
     ctx.beginPath();
 
     var i = cloth.points.length;
     // Loop over all the points and draw each point
     while (i--) 
-        cloth.points[i].draw();
+        draw_points(cloth.points[i]);
 
     ctx.stroke();
 };
 
-
-//------ Changed Function---------------
-//------- MOVED TO SERVER --------
-// update the cloth based on mouse events being performed on screen
-// function update_cloth(cloth_local) {
-// 	var i = parameters.physics_accuracy;
-
-//     // Resolve the constraints for all the points physics_accuracy number of times
-//     while (i--) {
-//         var p = cloth_local.points.length;
-//         while (p--)
-//             resolve_constraints(cloth_local.points[p]); 
-//     }
-
-//     i = cloth_local.points.length;
-    
-//     // update all the points by delta amount.
-//     while (i--) 
-//         update_point(cloth_local.points[i], .016);
-
-//     return cloth_local;
-// };
-
-var t = new timer();
-var startTime;
-
-// update the simulation
-function update_simulation() {
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // call this method on the server side
-    //cloth = update_cloth(cloth);
-    console.log('Emitting update event');
-    var compressed = msgpack.encode(cloth);//JSON.stringify(cloth);
-    socket.emit('update_cloth', {'point' : compressed});
-    // for(var i=0;i<cloth.points.length;i++){
-    //     console.log('Emitting ' + i);
-    //     socket.emit('update_cloth', {'point' : cloth.points[i]});
-    // }
-    console.log('Emit successful');
-    //socket.on('cloth_updated', function(data){
-
-    //});
-    //cloth.update();
-    //cloth.draw();
-    var end = new Date().getTime();
-    //console.log("Update finished: Time taken " + (end-start));
-    requestAnimFrame(update_simulation);
-    t.tick(new Date().getTime());
-    if(end-startTime > 1000){
-        startTime = new Date().getTime();
-        document.getElementById('fps').innerHTML = "FPS: " + t.fps();
+function draw_points(point){
+    if(point.constraints.length === 0)
+        return;
+    var i = point.constraints.length;
+    while(i--){
+        ctx.moveTo(point.x, point.y);
+        ctx.lineTo(point.constraints[i].x, point.constraints[i].y);    
     }
-
     
 }
-
-
 // start the simulation
 function start() {
 
@@ -312,5 +175,6 @@ window.onload = function () {
         event.preventDefault();
     }, false);
 
-    start();
+    load_variables();  
+    //start();
 };
