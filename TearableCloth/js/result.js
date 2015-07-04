5f4f84019b6184c25f4260a90ce1a868f924c3a8
varun saravagi <varunsaravagi@gmail.com>
2015-07-04 15:06:29 -0400

    new result.js for client side

--- TearableCloth/js/result.js
+++ TearableCloth/js/result.js
@@ -1,34 +1,48 @@
 function dataPoints(){
-  this.elatency = 0,
-  this.nlatency = 0,
-  this.fps = 0,
-  this.bandwidth = 0,
-  this.counter = 1
+  this.elatency = 0;
+  this.nlatency = 0;
+  this.fps = 0;
+  this.bandwidth = 0;
+  this.counter = 1;
+  this.elatencyStore = [];
+  this.nlatencyStore = [];
+  this.fpsStore = [];
+  this.bandwidthStore = [];
 }
 
 dataPoints.prototype = {
-  add : function(elatency, nlatency, fps, bandwidth){
+  add : function(elatency, fps){
     this.elatency += elatency;
-    this.nlatency += nlatency;
-    this.bandwidth += bandwidth;
+    //this.nlatency += nlatency;
+    //this.bandwidth += bandwidth;
     this.fps += fps;
+    this.elatencyStore.push(elatency);
+    this.fpsStore.push(fps);
     this.counter += 1;
   },
 
   avElatency : function(){
-    return Math.round(this.elatency/this.counter);
+    r = Math.round(this.elatency/this.counter);
+    this.elatencyStore.push(r);
+    return r;
   },
 
   avNlatency : function(){
-    return Math.round(this.nlatency/this.counter);
+    r = Math.round(this.nlatency/this.counter);
+    this.nlatencyStore.push(r);
+    return r;
   },
 
   avFps : function(){
-    return Math.round(this.fps/this.counter);
+    r = Math.round(this.fps/this.counter);
+    this.fpsStore.push(r);
+    return r;
   },
 
   avBandwidth : function(){
-    return Math.round(this.bandwidth/this.counter);
+    r = Math.round(this.bandwidth/this.counter);
+    this.bandwidthStore.push(r);
+    return r;
   },
 
   reset : function(){
@@ -37,5 +51,21 @@ dataPoints.prototype = {
     this.fps = 0;
     this.bandwidth = 0;
     this.counter = 1;
+  },
+
+  getSElatency : function(){
+    return this.elatencyStore;
+  },
+
+  getSNlatency : function(){
+    return this.nlatencyStore;
+  },
+
+  getSFps : function(){
+    return this.fpsStore;
+  },
+
+  getSBandwidth : function(){
+    return this.bandwidthStore;
   }
 }
